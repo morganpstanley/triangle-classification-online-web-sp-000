c17f9faa70ce427c7c491978ccbc88dcc35ae082
Morgan Stanley <morganpstanley@gmail.com>
2019-07-18 07:32:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -4,7 +4,16 @@ class Triangle
     @side_1 = side_1
     @side_2 = side_2
     @side_3 = side_3
+    @sides = []
+    @sides << side_1
+    @sides << side_2
+    @sides << sides_3
+    raise_error?
   end
+
+  def raise_error
+    if @sides.any?(0)
+    elsif @sides.any?{|side| side < 1}
 
   def kind
     if @side_1 == @side_2 && @side_1 == @side_3
