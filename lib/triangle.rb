720bb51db00c4bfaf664e6e276727ed6b284669d
Morgan Stanley <morganpstanley@gmail.com>
2019-07-18 07:50:10 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,42 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @sides = []
+    @sides << side_1
+    @sides << side_2
+    @sides << side_3
+    if raise_error? == true
+      raise TriangleError
+    else
+      kind
+    end
+  end
+
+  def raise_error?
+    if @sides.any?(0)
+      true
+    elsif @sides.any?{|side| side < 0.1}
+      true
+    elsif @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1
+      true
+    end
+  end
+
+  def kind
+    if @side_1 == @side_2 && @side_1 == @side_3
+      :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < TypeError
+  end
 end
