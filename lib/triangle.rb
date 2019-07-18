609c40f7078f8f211f069dafdadfc93f10a78c48
Morgan Stanley <morganpstanley@gmail.com>
2019-07-18 07:22:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,9 +7,9 @@ class Triangle
   end
 
   def kind
-    if side_1 == side_2 && side_1 == side_3
+    if @side_1 == @side_2 && @side_1 == @side_3
       equilateral:
-    elsif side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
       isosceles:
     else
       scalene:
